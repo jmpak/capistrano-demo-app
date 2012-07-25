c39fa215d9a8e775ea47beea57e61a098c35b69a
jmpak <jmpak80@gmail.com>
2012-07-25 15:02:01 +0800

    basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapistranoDemoApp::Application.config.secret_token = '21c582cc82f98179a2d79956356eed630fe650f428776855e1a9744b76f4b6a85abe150c814dff59aaac6c53d279cf9cfd414483f3d21939c39af2668108349e'
