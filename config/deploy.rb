caa9b6eaa1819769c92d30a4c914a6ae5f12f526
jmpak <jmpak80@gmail.com>
2012-07-25 16:10:08 +0800

    changed deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,16 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "capistrano-demo-app.jmpak.com"
+set :user, "vagrant"
+set :repository,  "https://github.com/jmpak/capistrano-demo-app.git"
 
-set :scm, :subversion
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :domain, application
+role :web, domain                                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+set :mongrel_username, 3
+set :mongrel_port, 8000
+set :rails_env, 'production'
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
