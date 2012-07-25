b2db650c5083f7b55902738980b11b7f353746c0
jmpak <jmpak80@gmail.com>
2012-07-25 16:43:01 +0800

    added peepserver plugin

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,10 +5,13 @@ set :repository,  "https://github.com/jmpak/capistrano-demo-app.git"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
+set :deploy_to, "/var/www/apps/#{application}"
 set :domain, application
 role :web, domain                                          # Your HTTP server, Apache/etc
 role :app, domain                          # This may be the same as your `Web` server
 role :db,  domain, :primary => true # This is where Rails migrations will run
+
+set :runner, user
 
 set :mongrel_username, 3
 set :mongrel_port, 8000
